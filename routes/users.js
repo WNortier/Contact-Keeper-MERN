df825ee86271d7b63b5e976c31597c97604d2ee4
wnortier <warwick.nortier@gmail.com>
2021-10-23 17:22:28 +0200

    authenticate route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
-const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+
+const User = require('../models/User');
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
@@ -34,6 +35,8 @@ router.post(
         email,
         password,
       });
+
+      console.log(user);
 
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
