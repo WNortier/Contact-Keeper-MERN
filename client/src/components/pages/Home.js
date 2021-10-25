5a996d7176cbc80dbaf6ef3422bb5e1716ed6e03
wnortier <warwick.nortier@gmail.com>
2021-10-25 17:05:57 +0200

    contacts & contactitem components

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Contacts from '../contacts/Contacts';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>This is the home page</h1>
+    <div className="grid-2">
+      <div></div>
+      <div>
+        <Contacts></Contacts>
+      </div>
     </div>
   );
 };
